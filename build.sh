d43e634e008016b7b4a977f9780b40ef06831423
Agus Suristyono <sandatjepil@proton.me>
2025-05-30 08:48:39 +0700

    LLVM: 20.1.5
    * Compile Binutils
    * Use builtin zstd (as I compiled manually in the custom docker image)
    * Use Ubuntu 22.04 Jammy Jellyfish for GLibC standards

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-export TZ="Asia/Jakarta"
 export LLVM_NAME="Kaleidoscope"
-export STABLE_TAG="llvmorg-20.1.1"
-export INSTALL="${PWD}/install"
+export STABLE_TAG="llvmorg-20.1.5"
+export HOME_DIR="$(pwd)"
+export INSTALL_DIR="${HOME_DIR}/install"
 export CHAT_ID="$TELEGRAM_CHAT"
-export BUILD_DATE="$(date +%Y%m%d)"
-export BUILD_DAY="$(date "+%d %B %Y")"
-export BUILD_TAG="$(date +%Y%m%d-%H%M-%Z)"
-export NPROC="$(nproc --all)"
+export BUILD_DATE="$(date "+%Y%m%d")"
+export BUILD_DAY="$(date "+%d %B %Y, %H:%M %Z")"
+export BUILD_TAG="$(date "+%Y%m%d-%H%M-%Z")"
+export NPROC=8
 export CUSTOM_FLAGS="
   LLVM_PARALLEL_TABLEGEN_JOBS=${NPROC}
   LLVM_PARALLEL_COMPILE_JOBS=${NPROC}
@@ -21,19 +21,21 @@ export CUSTOM_FLAGS="
   CMAKE_STATIC_LINKER_FLAGS='-Wl,-O3,--lto-O3,--lto-CGO3,--gc-sections,--strip-debug'
   "
 
-FINAL=false
-RELEASE=false
+[ -d "${INSTALL_DIR}" ] || mkdir -p "${INSTALL_DIR}"
+
+NOTIFY=false
+FINAL=false RELEASE=false
+BINUT=false PUSHING=false
 for ARGS in $@; do
   case $ARGS in
-  final)
-    FINAL=true
-    ;;
-  release)
-    RELEASE=true
-    ;;
+  msg) NOTIFY=true ;;
+  final) FINAL=true ;;
+  release) RELEASE=true ;;
+  binut) BINUT=true ;;
+  push) PUSHING=true ;;
   esac
 done
-export FINAL RELEASE
+export NOTIFY FINAL RELEASE BINUT PUSHING
 
 send_info() {
   curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage \
@@ -51,9 +53,6 @@ send_file() {
 }
 
 build_llvm() {
-  send_info "GitHub Action : " "Building LLVM . . ."
-  BUILD_START=$(date +"%s")
-
   if ${FINAL}; then
     ADD="${ADD} --final"
   fi
@@ -64,26 +63,38 @@ build_llvm() {
 
   ./build-llvm.py ${ADD} \
     --build-type "Release" \
-    --build-stage1-only \
     --defines "${CUSTOM_FLAGS}" \
-    --install-folder "${INSTALL}" \
+    --install-folder "${INSTALL_DIR}" \
     --lto thin \
-    --pgo llvm \
+    --pgo kernel-defconfig-slim \
     --projects clang lld polly \
     --shallow-clone \
     --targets AArch64 ARM X86 \
     --no-update \
     --vendor-string "${LLVM_NAME}" |& tee -a build.log
-  BUILD_END=$(date +"%s")
-  DIFF=$((BUILD_END - BUILD_START))
 
   # Check LLVM files
-  if [ -f ${INSTALL}/bin/clang ] || [ -f ${PWD}/build/llvm/instrumented/profdata.prof ]; then
-    send_info "GitHub Action : " "LLVM compilation finished ! ! !"
-    send_info "Time taken : " "$((DIFF / 60))m $((DIFF % 60))s"
+  if [ -f ${INSTALL_DIR}/bin/clang ]; then
+    send_info "Action : " "LLVM compilation finished ! ! !"
+  elif [ "$FINAL" = "false" ] && [ -n "$(ls -A ${HOME_DIR}/build/llvm/instrumented 2>/dev/null)" ]; then
+    send_info "Action : " "Instrumented LLVM compilation finished ! ! !"
   else
-    send_info "GitHub Action : " "LLVM compilation failed ! ! !"
+    send_info "Action : " "LLVM compilation failed ! ! !"
     send_file "LLVM build.log" ./build.log
+    exit 1
+  fi
+}
+
+build_binutils() {
+  ./build-binutils.py \
+    --install-folder "${INSTALL_DIR}" \
+    --targets aarch64 arm x86_64 | tee -a build.log
+    
+  if ls "${INSTALL_DIR}"/bin/aarch64-linux* >/dev/null 2>&1; then
+    send_info "Action: " "Binutils compilations finished"
+  else
+    send_info "Action: " "Binutils compilations failed"
+    send_file "Binutils build.log" ./build.log
     exit 1
   fi
 }
@@ -91,15 +102,15 @@ build_llvm() {
 build_zstd() {
   git clone https://github.com/facebook/zstd -b v1.5.7 --depth=1
   cd zstd
-  cmake build/cmake -DCMAKE_INSTALL_PREFIX="${INSTALL}/.zstd" |& tee -a build.log
+  cmake build/cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}/.zstd" |& tee -a build.log
   make -j${NPROC} |& tee -a build.log
   make install -j${NPROC} |& tee -a build.log
   cd -
 }
 
 strip_binaries() {
-  find ${INSTALL} -type f -exec file {} \; >.file-idx
-  cp ${INSTALL}/bin/llvm-objcopy ./strip
+  find ${INSTALL_DIR} -type f -exec file {} \; >.file-idx
+  cp ${INSTALL_DIR}/bin/llvm-objcopy ./strip
   grep "not strip" .file-idx |
     tr ':' ' ' | awk '{print $1}' |
     while read -r file; do ./strip --strip-all-gnu "${file}"; done
@@ -109,37 +120,49 @@ strip_binaries() {
 }
 
 git_release() {
-  CLANG_VERSION="$(${INSTALL}/bin/clang --version | head -n1 | cut -d ' ' -f4)"
+  CLANG_VERSION="$(${INSTALL_DIR}/bin/clang --version | grep -oP '\d+\.\d+(\.\w+)?' | head -n1)"
+  GLIBC_VERSION="$(ldd --version | grep -oP '\d+\.\d+(\.\d+)?' | head -n1)"
+  BINUT_VERSION="$($(ls ${INSTALL_DIR}/bin/*objdump 2>/dev/null | head -n1) --version | grep -oP '\d+\.\d+(\.\d+)?' | head -n1)"
   MESSAGE="Clang: ${CLANG_VERSION}-${BUILD_DATE}"
-  send_info "GitHub Action : " "Release into GitHub . . ."
-  send_info "Clang Version : " "${CLANG_VERSION}"
-  cd ${INSTALL}
-  tar -I"${INSTALL}/.zstd/bin/zstd --ultra -22 -T0" -cf clang.tar.zst *
+  cd ${INSTALL_DIR}
+  # tar -I"${INSTALL_DIR}/.zstd/bin/zstd --ultra -22 -T0" -cf clang.tar.zst *
+  tar -I "zstd --ultra -22 -T0" -cf clang.tar.zst *
+  TAR_SIZE="$(du -m clang.tar.zst | cut -f1)"
   cd ..
   git config --global user.name github-actions[bot]
   git config --global user.email github-actions[bot]@users.noreply.github.com
-  git clone https://sandatjepil:${GITHUB_TOKEN}@github.com/purrrslitterbox/clang-releases.git clang -b main
+  git clone https://sandatjepil:${GITHUB_TOKEN}@github.com/PurrrsLitterbox/LLVM-weekly.git clang -b main
   cd clang
-  cat README |
-    sed s/LLVM_VERSION/${CLANG_VERSION}-${BUILD_DATE}/g |
-    sed s/SIZE/$(du -m ${INSTALL}/clang.tar.zst | cut -f1)/g >README.md
-  git commit --allow-empty -asm "${MESSAGE}"
+  sed -e "s/GLIBC_VER/${GLIBC_VERSION}/" \
+    -e "s/LLVM_VERSION/${CLANG_VERSION}/" \
+    -e "s/SIZE/${TAR_SIZE}/" \
+    -e "s/BINUTILS_VER/${BINUT_VERSION}/" \
+    README > README.md
+  echo "https://github.com/PurrrsLitterbox/LLVM-weekly/releases/download/${BUILD_TAG}/clang.tar.zst" > latestlink.txt
+  send_info "Action : " "Release into GitHub . . ."
+  send_info "Clang Version : " "${CLANG_VERSION}"
+  git add . && git commit --allow-empty -sm "${MESSAGE}"
   git push origin main
-  cp ${INSTALL}/clang.tar.zst .
-  hub release create -a clang.tar.zst -m "${MESSAGE}" ${BUILD_TAG}
-  send_info "GitHub Action : " "Toolchain released ! ! !"
+  cp ${INSTALL_DIR}/clang.tar.zst .
+  hub release create -a clang.tar.zst -m "${MESSAGE}
+
+$(cat README.md)" "${BUILD_TAG}"
+  send_info "Action : " "Toolchain released ! ! !"
   cd ..
 }
 
-TOTAL_START=$(date +"%s")
-send_info "Date : " "${BUILD_DAY}"
-send_info "GitHub Action : " "Toolchain compilation started . . ."
-build_llvm
-if ${FINAL}; then
-  build_zstd
+if "${NOTIFY}"; then
+  send_info "Date: " "$BUILD_DAY"
+  send_info "Action: " "LLVM Compilation Started"
+fi
+if ${RELEASE}; then
+  build_llvm
+fi
+if ${BINUT}; then
+  build_binutils
+fi
+if ${PUSHING}; then
+  # build_zstd
   strip_binaries
   git_release
 fi
-TOTAL_END=$(date +"%s")
-DIFF=$((TOTAL_END - TOTAL_START))
-send_info "Total CI operation : " "$((DIFF / 60))m $((DIFF % 60))"
